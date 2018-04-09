2a83a396bd8cd2220ffaa8386f61a59889107d97
softiodev <ecosmer@outlook.com>
2018-04-09 10:57:25 -0400

    Creciendo tamano de resolucion del programa

--- Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/Form1.Designer.vb
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/Form1.Designer.vb
@@ -29,7 +29,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(378, 308)
+        Me.ClientSize = New System.Drawing.Size(542, 474)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
