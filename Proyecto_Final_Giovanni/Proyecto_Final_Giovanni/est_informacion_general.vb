d7eae1210710f5ab5f3c1836116d0e52d63d5a4a
softiodev <ecosmer@outlook.com>
2018-04-11 09:48:45 -0400

    El form de Eddie Rivera: Informacion general de los estudiantes

---
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/est_informacion_general.vb
@@ -0,0 +1,6 @@
+﻿Public Class informacion_general_est
+    Private Sub informacion_general_est_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size = variables_globales.tamano
+        Me.StartPosition = FormStartPosition.CenterScreen
+    End Sub
+End Class
