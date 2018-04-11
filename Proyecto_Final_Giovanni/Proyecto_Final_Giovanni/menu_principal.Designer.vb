7a9c27fd03753ec0acc24dfda63b9d8d810ce4c9
softiodev <ecosmer@outlook.com>
2018-04-11 09:51:55 -0400

    Form de Eddie Rivera. Menu principal. Lo hice yo, Edwin, porque GitHub no recibi¢ los archivos de Eddie.

---
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/menu_principal.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class menu_principal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(menu_principal))
+        Me.tb_usuario = New System.Windows.Forms.TextBox()
+        Me.tb_contrasena = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_acceder = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tb_usuario
+        '
+        Me.tb_usuario.Location = New System.Drawing.Point(221, 148)
+        Me.tb_usuario.Name = "tb_usuario"
+        Me.tb_usuario.Size = New System.Drawing.Size(100, 20)
+        Me.tb_usuario.TabIndex = 0
+        '
+        'tb_contrasena
+        '
+        Me.tb_contrasena.Location = New System.Drawing.Point(221, 235)
+        Me.tb_contrasena.Name = "tb_contrasena"
+        Me.tb_contrasena.Size = New System.Drawing.Size(100, 20)
+        Me.tb_contrasena.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 148)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(74, 242)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Contraseña"
+        '
+        'btn_acceder
+        '
+        Me.btn_acceder.Location = New System.Drawing.Point(221, 303)
+        Me.btn_acceder.Name = "btn_acceder"
+        Me.btn_acceder.Size = New System.Drawing.Size(100, 23)
+        Me.btn_acceder.TabIndex = 4
+        Me.btn_acceder.Text = "Acceder"
+        Me.btn_acceder.UseVisualStyleBackColor = True
+        '
+        'menu_principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(357, 461)
+        Me.Controls.Add(Me.btn_acceder)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_contrasena)
+        Me.Controls.Add(Me.tb_usuario)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "menu_principal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tb_usuario As TextBox
+    Friend WithEvents tb_contrasena As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_acceder As Button
+End Class
---
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/menu_principal.vb
@@ -0,0 +1,34 @@
+﻿Public Class menu_principal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim usuario As TextBox = tb_usuario 'Textbox del nombre de usuario
+        Dim pwd As TextBox = tb_contrasena 'Textbox de la contraseña
+
+        Me.Size = variables_globales.tamano 'Seteando qué tan grande se verá el programa cuando lo compiles
+
+        usuario.Text = "admin"
+        pwd.Text = "password_admin"
+
+    End Sub
+
+    Private Sub btn_acceder_Click(sender As Object, e As EventArgs) Handles btn_acceder.Click
+        Dim usuario As String = tb_usuario.Text 'Textbox del nombre de usuario
+        Dim pwd As String = tb_contrasena.Text 'Textbox de la contraseña
+
+        'TAREA PARA HACER
+        'Añadir codigo de base de datos aqui
+        'para que el acceso a la cuenta 
+        'funcione con base de datos
+
+        If (usuario = "admin" And pwd = "password_admin") Then
+            MsgBox("Informacion valida" & Environment.NewLine &
+                   "Bienvenido '" & usuario & "'")
+            Visible = False 'Hacer que este Form no se vea
+            'TAREA PARA HACER 
+            'Como Eddie todavia no ha hecho su parte,
+            'lo pongo en comentario porque en su Form
+            'debe haber un boton al proximo Form: NOTAS DEL ESTUDIANTE
+            'Cuando Eddie haga eso, se quita este comentario
+            'informacion_general_est.Show() 'Haciendo que el nuevo Form aparezca
+        End If
+    End Sub
+End Class
