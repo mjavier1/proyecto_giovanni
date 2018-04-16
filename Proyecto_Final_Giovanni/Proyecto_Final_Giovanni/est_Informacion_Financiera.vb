ddbafa27062fd4cc6c9b674c88fabe249377974a
softiodev <ecosmer@outlook.com>
2018-04-16 09:10:05 -0400

    Cambiando valor del TextBox de TOTAL CREDITOS de 15 a 12

--- Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/est_Informacion_Financiera.vb
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/est_Informacion_Financiera.vb
@@ -6,7 +6,7 @@
     Private Sub est_Informacion_Financiera_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         codigo_semestre.Text = "2018-10 (Ago-Dec 2017)"
         total_beca.Text = 2500
-        total_creditos.Text = 15
+        total_creditos.Text = 12
         total_sub_balance.Text = Val(total_creditos.Text) * 185
         total_balance.Text = Val(total_beca.Text) - Val(total_sub_balance.Text)
     End Sub
