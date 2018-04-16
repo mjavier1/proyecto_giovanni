299c895b55631b0bd7177a173f8ef7fd5dcd5ff5
ERF2018 <38217400+ERF2018@users.noreply.github.com>
2018-04-16 08:26:49 -0400

    Add files via upload

---
+++ Proyecto/Proyecto/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Welcome to InterAmericana Student Informational System")
+        Form2.Show()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
