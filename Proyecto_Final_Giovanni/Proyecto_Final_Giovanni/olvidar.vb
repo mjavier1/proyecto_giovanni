35e5d3289c935d77418cb7c1234904c510ab2fc5
softiodev <ecosmer@outlook.com>
2018-04-26 09:45:34 -0400

    Fixing bugs

--- Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/olvidar.vb
+++ Proyecto_Final_Giovanni/Proyecto_Final_Giovanni/olvidar.vb
@@ -22,4 +22,8 @@ Public Class olvidar
             MsgBox("Informacion no fue encontrada. ")
         End If
     End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
 End Class
