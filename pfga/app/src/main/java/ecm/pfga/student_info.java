217eefd7b9df9a70ed9040b1b36ca216774326cf
softiodev <ecosmer@outlook.com>
2018-05-05 10:18:33 -0400

    Added student form

--- pfga/app/src/main/java/ecm/pfga/student_info.java
+++ pfga/app/src/main/java/ecm/pfga/student_info.java
@@ -1,7 +1,10 @@
 package ecm.pfga;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class student_info extends Activity {
 
@@ -9,5 +12,16 @@ public class student_info extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
+
+        Button btn_submitInfo = findViewById(R.id.btn_submit);
+        btn_submitInfo.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(student_info.this, student_activities.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
