6bd59adba1a36ec6bf22163d86cdb5bbbd242690
softiodev <ecosmer@outlook.com>
2018-05-05 10:15:01 -0400

    Fixing bugs

---
+++ pfga/app/src/main/java/ecm/pfga/student_activities.java
@@ -0,0 +1,13 @@
+package ecm.pfga;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class student_activities extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_activities);
+    }
+}
