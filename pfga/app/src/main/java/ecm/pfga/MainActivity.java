7026f1b84f2972987a59d0c48a133876f3ac5a52
softiodev <ecosmer@outlook.com>
2018-05-05 10:17:24 -0400

    Added login menu

--- pfga/app/src/main/java/ecm/pfga/MainActivity.java
+++ pfga/app/src/main/java/ecm/pfga/MainActivity.java
@@ -1,7 +1,10 @@
 package ecm.pfga;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, student_info.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
