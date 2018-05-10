c699f494ec9cfa4ba0223c13238d7d8462cf5625
softiodev <ecosmer@outlook.com>
2018-05-10 18:06:48 -0400

    App (on smartphone) is now registering student on database

--- pfga/app/src/main/java/ecm/pfga/Constants.java
+++ pfga/app/src/main/java/ecm/pfga/Constants.java
@@ -1,6 +1,6 @@
 package ecm.pfga;
 
 public class Constants {
-    public static final String ROOT_URL = "192.168.1.100/Android/v1/";
-    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+    public static final String ROOT_URL = "http://192.168.43.214/php/v1/";
+    public static final String URL_REGISTER = ROOT_URL + "registeruser.php";
 }
