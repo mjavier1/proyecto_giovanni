268d34be39e9b5314464b26e6104671c2c34e01e
softiodev <ecosmer@outlook.com>
2018-05-14 07:57:03 -0400

    Saving user data

--- pfga/app/src/main/java/ecm/pfga/SharedPreferenceManager.java
+++ pfga/app/src/main/java/ecm/pfga/SharedPreferenceManager.java
@@ -1,6 +1,7 @@
 package ecm.pfga;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -8,12 +9,15 @@ import com.android.volley.toolbox.Volley;
 
 public class SharedPreferenceManager {
     private static SharedPreferenceManager mInstance;
-    private RequestQueue mRequestQueue;
-    private static Context mCtx;
 
+    private static Context mCtx;
+    private static final String SHARED_PREF_NAME = "mysharedpref12";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_USER_EMAIL = "useremail";
+    private static final String KEY_USER_ID = "userid";
     private SharedPreferenceManager(Context context){
         mCtx = context;
-        mRequestQueue = getRequestQueue();
+
     }
 
     public static synchronized SharedPreferenceManager getInstance(Context context) {
@@ -23,16 +27,7 @@ public class SharedPreferenceManager {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null){
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
-        }
-        return mRequestQueue;
-    }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
+    public boolean userLogin(int id, String username, String email){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences()
     }
 }
